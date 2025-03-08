53314470cabadc93752036ce14048ee44836261c
Pablo Castano <pablocastao@microsoft.com>
2025-03-08 09:54:21 -0300

    first commit

--- src/agent.py
+++ src/agent.py
@@ -5,28 +5,17 @@ from typing import List, Any, Optional
 from azure.identity.aio import DefaultAzureCredential
 import asyncio
 
+
 class Agent:
     def __init__(self, deployment_name: str, connection_string: str) -> None:
-        self.deployment_name = deployment_name
-        self.connection_string = connection_string
-
-    async def __aenter__(self) -> "Agent":
-        self.ai_agent_settings = AzureAIAgentSettings.create(model_deployment_name=self.deployment_name)
+        self.ai_agent_settings = AzureAIAgentSettings.create(
+            model_deployment_name=deployment_name
+        )
         self.credential = DefaultAzureCredential()
         self.client = AzureAIAgent.create_client(
             credential=self.credential,
-            conn_str=self.connection_string,
+            conn_str=connection_string,
         )
-        return self
-
-    async def __aexit__(
-        self, 
-        exc_type: Optional[type], 
-        exc_val: Optional[BaseException], 
-        exc_tb: Optional[Any]
-    ) -> None:
-        await self.credential.close()
-        await self.client.close()
 
     async def create_agent(
         self,
@@ -42,4 +31,3 @@ class Agent:
             tool_resources=tool["resources"],
         )
         return AzureAIAgent(client=self.client, definition=agent_definition)
-
